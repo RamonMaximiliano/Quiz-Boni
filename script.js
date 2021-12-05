801f7b97b648b5f17ae3fbc47c27d140ccaee1af
Ramon Maximiliano Wilbert <ramon_max1986@hotmail.com>
2021-12-05 12:58:05 -0300

    Starting quiz

---
+++ script.js
@@ -0,0 +1,23 @@
+
+
+
+
+
+
+
+
+/* function showImage() {
+    //pegou o arquivo e colocou numa variavel
+    let image = document.getElementById("image").files[0];
+    console.log(image)
+
+    //criou o elemento tag img
+    let img = document.createElement("img");
+    //criou o atributo source com uma URL
+    img.src = URL.createObjectURL(image)
+    //definiu a width da imagem
+    img.width = 200;
+
+    //inseriu a imagem como child da div
+    document.getElementById("divImage").appendChild(img)
+}   */
